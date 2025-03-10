9f763d9b846c90e82101e6008e5fcaac803ac306
WMH <wmh_1012@126.com>
2025-03-09 23:29:00 -0400

    new algo

--- mfglib/alg/__init__.py
+++ mfglib/alg/__init__.py
@@ -3,9 +3,12 @@ from mfglib.alg.mf_omo import MFOMO
 from mfglib.alg.occupation_measure_inclusion import OccupationMeasureInclusion
 from mfglib.alg.online_mirror_descent import OnlineMirrorDescent
 from mfglib.alg.prior_descent import PriorDescent
+from mfglib.alg.AID_fictitious_play import AIDFictitiousPlay
+
 
 __all__ = [
     "FictitiousPlay",
+    "AIDFictitiousPlay",
     "MFOMO",
     "OnlineMirrorDescent",
     "PriorDescent",
